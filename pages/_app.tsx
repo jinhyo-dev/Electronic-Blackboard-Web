7428083d28e2f8cbd0114dc16ec2096b8de473c3
jinhyo-dev <jinhyo0850@gmail.com>
2022-04-22 11:55:26 +0900

    notice.tsx style changed 4/22 11:55

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/footer.css'
 import '../styles/food.css'
+import '../styles/notice.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
